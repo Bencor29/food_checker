177da0b9ba41a27c9abedd1d4f61ddb0c27bdaee
Bencor29 <benjamin@cornou.dev>
2021-11-18 11:18:48 +0100

    Test server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const sequelize = require('./config').db;
-const Ingredient = require('./models/db/ingredient');
+const Brand = require('./models/db/brand');
 require('./models/db/__relations');
 
 (async () => {
     await sequelize.sync();
+    Brand.findAll().then(brands => {
+        console.log('Brands:');
+        brands.forEach(brand =>  {
+            console.log(' - ' + brand.name);
+        });
 
+        sequelize.close();
+    })
 })();
